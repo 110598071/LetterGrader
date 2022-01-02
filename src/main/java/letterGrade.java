ac12cafacdfbc2d076ff6b72391ed84b4eada4c7
110598071 <t110598071@ntut.org.tw>
2022-01-02 22:23:23 +0800

    add main function

--- src/main/java/letterGrade.java
+++ src/main/java/letterGrade.java
@@ -1,26 +1,45 @@
-public class letterGrade {
-    public char getScoreLetter(int score) {
-        if (score < 0 || score > 100) {
-            result = 'X';
-        }
-        else if (score >= 90) {
-            result = 'A';
-        }
-        else if (score >= 80) {
-            result = 'B';
-        }
-        else if (score >= 70) {
-            result = 'C';
-        }
-        else if (score >= 60) {
-            result = 'D';
-        }
-        else {
-            result = 'F';
-        }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-        return result;
+public class letterGrade {
+    public static void main(String[] args) {
+        int score;
+        System.out.print("Enter the score = ");
+        try {
+            InputStreamReader isr = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(isr);
+            score = Integer.parseInt(br.readLine());
+            char grade = getScoreLetter(score);
+            System.out.println("The grade of " + score + " is " + grade);
+        } catch (NumberFormatException ex) {
+            System.out.println("Not an Integer!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private char result;
+    public static char getScoreLetter(int score) {
+        char grade;
+        if (score < 0 || score > 100) {
+            grade = 'X';
+        }
+        else if (score >= 90) {
+            grade = 'A';
+        }
+        else if (score >= 80) {
+            grade = 'B';
+        }
+        else if (score >= 70) {
+            grade = 'C';
+        }
+        else if (score >= 60) {
+            grade = 'D';
+        }
+        else {
+            grade = 'F';
+        }
+
+        return grade;
+    }
 }
